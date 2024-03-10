06fa9d49d1ca7b41c1b0de38cd2bccf02fdd5f0e
Kez Anwar <kezanwar@gmail.com>
2024-03-10 15:09:33 +0000

    feature/setup - finished initial layout of Account API

--- Makefile
+++ Makefile
@@ -4,5 +4,9 @@ build:
 run:
 	@./bin/bank-api
 
+dev: 	
+	@go build -o bin/bank-api
+	@./bin/bank-api
+
 test: 
 	@go test -v ./..
--- api.go
+++ api.go
@@ -1,33 +1,91 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type APIServer struct {
-	listernAddr string
+	listenAddr string
 }
 
 func NewApiServer(listenAddr string) *APIServer {
 	return &APIServer{
-		listernAddr: listenAddr,
+		listenAddr: listenAddr,
 	}
 }
 
-func (server *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+type ApiHandler func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandler(f ApiHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			// handle the error
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandler(s.handleAccount))
+
+	log.Println("Bank API running on port", s.listenAddr)
+
+	http.ListenAndServe(s.listenAddr, router)
+
 	return nil
 }
 
-func (server *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("method not allow %s", r.Method)
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+
+	account := NewAccount("Kez", "Anwar")
+
+	WriteJSON(w, http.StatusOK, account)
+
 	return nil
 }
 
-func (server *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (server *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (server *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
